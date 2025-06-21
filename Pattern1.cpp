54c30bd6c2c194e2e50a3bb0848d77b546066bd6
Sheilendra SIngh <sheilendra31@gmail.com>
2025-06-21 22:31:31 +0530

    Pattern 1

---
+++ Pattern1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    printPattern(num);
+    return 0;
+}
