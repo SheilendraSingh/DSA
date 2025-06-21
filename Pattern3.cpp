bfda3854321ccb09fcc25c11d26a8e3d8e4e842a
Sheilendra SIngh <sheilendra31@gmail.com>
2025-06-21 22:37:33 +0530

    Pattern3

---
+++ Pattern3.cpp
@@ -0,0 +1,31 @@
+/*
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    printPattern(num);
+    return 0;
+}
