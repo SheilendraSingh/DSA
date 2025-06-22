931d26cc94078c9124b70e7cccf7b8919045ab1d
Sheilendra SIngh <sheilendra31@gmail.com>
2025-06-22 09:29:07 +0530

    Pattern 8

---
+++ Pattern8.cpp
@@ -0,0 +1,39 @@
+/*
+ * * * * * * * * *
+ * * * * * * *
+ * * * * *
+ * * *
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = num; j > i; j--)
+        {
+            cout << "*";
+        }
+        for (int j = num; j > i + 1; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    printPattern(num);
+    return 0;
+}
