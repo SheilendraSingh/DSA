e3d3bd2e315e2d56f9b66f40f1d24de4eb75d8a9
Sheilendra SIngh <sheilendra31@gmail.com>
2025-07-01 11:21:27 +0530

    Largest Sum in the array with sum zero GeeksForGeeks

---
+++ LargestSubarrayWithSumZero.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LarSubWithSumZero(vector<int> &arr)
+{
+
+    int n = arr.size();
+    unordered_map<int, int> mpp;
+    mpp[0] = -1;
+    int res = 0;
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        if (sum == 0)
+        {
+            res = i + 1;
+        }
+        else if (mpp.find(sum) != mpp.end())
+        {
+            res = max(res, i - mpp[sum]);
+        }
+        else
+        {
+            mpp[sum] = i;
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter thesize of the array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the elements in the array: ";
+
+    for (int &val : arr)
+    {
+        cin >> val;
+    }
+
+    cout << LarSubWithSumZero(arr);
+
+    return 0;
+}
