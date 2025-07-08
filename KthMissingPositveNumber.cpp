b7c5bb6a354565f2eacf0eca59b25770a656c462
Sheilendra SIngh <sheilendra31@gmail.com>
2025-07-08 23:45:29 +0530

    LeetCode #1539

---
+++ KthMissingPositveNumber.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findKthPositive(vector<int> &arr, int k)
+{
+    int left = 0, right = arr.size() - 1;
+
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        int missing = arr[mid] - (mid + 1);
+
+        if (missing < k)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return left + k;
+}
+
+int main()
+{
+
+    int n, k;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the Elments in the array: ";
+
+    for (int &val : arr)
+    {
+        cin >> val;
+    }
+
+    cout << findKthPositive(arr, k);
+    ;
+    return 0;
+}
